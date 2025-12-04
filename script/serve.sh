ec57a766ea2eb3740e90240af2dc87f132ffcdbe
Yanyiyi <ian@ocf.tw>
2025-12-05 00:44:25 +0800

    Update serve.sh
    Signed-off-by: Yanyiyi <ian@ocf.tw>

--- script/serve.sh
+++ script/serve.sh
@@ -9,7 +9,7 @@
 # (jekyll defaults to "origin" if a remote of that name exists,
 # which makes sense for a true fork, but not for most contributors)
 if [ "_${PAGES_REPO_NWO}_" == "__" ]; then
-export PAGES_REPO_NWO=standard-for-public-code/standard-for-public-code
+export PAGES_REPO_NWO=ocftw/standard-for-public-code
 fi
 
 bundle exec jekyll serve --livereload
